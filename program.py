f7ef77134f0c0a2b27e89cb9c507abf776ad06f4
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-09 15:13:49 +0000

    commnit

--- program.py
+++ program.py
@@ -7,7 +7,12 @@
 # End at 32
 
 result = ""
-for myNumber in range(32):
-  result = str(result) + str(myNumber) + "\n"
-
+for myNumber in range(1,33):
+  if ( myNumber % 15 == 0 ): 
+    print("FizzBuzz")
+  elif ( myNumber % 3 == 0 ): 
+    print("Fizz")
+  elif ( myNumber % 5 == 0 ): 
+    print("Buzz")
+  else: print(myNumber)
 print(result)
